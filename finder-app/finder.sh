fadc4c718f68f7284961e4a59c640d5c21afa7c4
Nzorro1978 <nzojihipolito@gmail.com>
2023-01-07 00:10:17 +0000

    username validation test passed

---
+++ finder-app/finder.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+###
+# finds an argument tofind within the filepath directories and subdirectories
+find()
+{
+    filepath=$1
+    tofind=$2
+    number_of_files=0
+    number_of_matches=0
+    [ -z "$filepath" ] && echo "File path not given!" && exit 1
+    [ -z "$tofind" ] && echo "No argument to search provided!" && exit 1
+    
+    [ ! -d "$filepath" ] && echo "$filepath is  not a directory"  && exit 1
+
+    for file in $(ls -R $filepath ); do
+        if [ -f "$file" ] ; then
+	    echo "$file"
+            matches="$(grep -i "$tofind" $file | wc -l)"
+            number_of_matches=$(( $number_of_matches + $matches ))
+            number_of_files=$(( $number_of_files + 1 ))
+	fi
+    done
+    echo "The number of files are $number_of_files and the number of matching lines are $number_of_matches"
+}
+
+find $1 $2
