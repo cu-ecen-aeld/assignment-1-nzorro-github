f1d561f7d3241038bc965751bcb04f46e346b7e7
Nzorro1978 <nzojihipolito@gmail.com>
2023-01-12 02:06:01 +0000

    Ready for Github

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -60,6 +60,6 @@ if [ $? -eq 0 ]; then
 	echo "success"
 	exit 0
 else
-	echo "failed: expected  ${MATCHSTR} in ${OUTPUTSTRING} but instead found"
+	echo "failed: expected  ${MATCHSTR} in output but instead found  ${OUTPUTSTRING}"
 	exit 1
 fi
